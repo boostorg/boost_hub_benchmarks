b3ed8faf1da68aa1224d2a8044bbb791291fec92
joaquintides <joaquin@tid.es>
2026-01-16 12:58:56 +0100

    compared forward and bidirectional available list

--- benchmark.cpp
+++ benchmark.cpp
@@ -149,6 +149,9 @@ void benchmark(const char* title, FHive fhive, FHub fhub)
   std::cout << std::string(41, '-') << "\n"
             << title << "\n"
             << "sizeof(element): " << sizeof(element) << "\n";
+#if defined(BOOST_HUB_ENABLE_BIDIRECTIONAL_AVAILABLE_LIST)
+  std::cout << "uses bidirectional available list\n";
+#endif
   std::cout << std::left << std::setw(11) << "" << "container size\n" << std::right
             << std::left << std::setw(11) << "erase rate" << std::right;
   for(std::size_t i = 3; i <= 7; ++i)
