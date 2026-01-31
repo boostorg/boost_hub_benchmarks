10dd61fbe41f48626737a6360566f216c82a0ba1
joaquintides <joaquin@tid.es>
2026-01-31 18:17:55 +0100

    implemented result table creation and uploading to repo

--- benchmark.cpp
+++ benchmark.cpp
@@ -56,11 +56,13 @@ void resume_timing()
 #include <boost/core/detail/splitmix64.hpp>
 #include <cmath>
 #include <cstring>
+#include <fstream>
 #include <iostream>
 #include <iomanip>
 #include <plf_hive.h>
 #include <stdexcept>
 #include <string>
+#include <sstream>
 #include <vector>
 
 struct element
@@ -150,40 +152,62 @@ void fill(Container& c, std::size_t n)
   }
 }
 
+static std::size_t min_size_exp = 3,
+                   max_size_exp = 7;
+static double      min_erasure_rate = 0.0,
+                   max_erasure_rate = 0.9,
+                   erase_rate_inc = 0.1;          
+
+struct benchmark_result
+{
+  std::string                           title;
+  std::vector<std::vector<std::string>> data;
+};
+
 template<typename FHive, typename FHub>
-void benchmark(const char* title, FHive fhive, FHub fhub)
+benchmark_result benchmark(const char* title, FHive fhive, FHub fhub)
 {
   static constexpr std::size_t size_limit =
     sizeof(std::size_t) == 4?  800ull * 1024ull * 1024ull:
                               2048ull * 1024ull * 1024ull;
+
+  benchmark_result res = {title};
 
   std::cout << std::string(41, '-') << "\n"
             << title << "\n"
             << "sizeof(element): " << sizeof(element) << "\n";
   std::cout << std::left << std::setw(11) << "" << "container size\n" << std::right
             << std::left << std::setw(11) << "erase rate" << std::right;
-  for(std::size_t i = 3; i <= 7; ++i)
+  for(std::size_t i = min_size_exp; i <= max_size_exp; ++i)
   {
     std::cout << "1.E" << i << " ";
   }
   std::cout << std::endl;
-  for(double erasure_rate = 0.0; erasure_rate <= 0.9; erasure_rate += 0.1) {
+  for(double erasure_rate = min_erasure_rate; 
+      erasure_rate <= max_erasure_rate; 
+      erasure_rate += erase_rate_inc) {
     std::cout << std::left << std::setw(11) << erasure_rate << std::right << std::flush;
-    for(std::size_t i = 3; i <= 7; ++i) {
-      std::size_t n = (std::size_t)std::pow(10.0, (double)i);
+
+    res.data.push_back({});
+
+    for(std::size_t i = min_size_exp; i <= max_size_exp; ++i) {
+      std::ostringstream out;
+      std::size_t        n = (std::size_t)std::pow(10.0, (double)i);
       if(n * sizeof(element) > size_limit) {
-        std::cout << "---- " << std::flush;
+        out << "----";
         continue;
       }
-
-      auto thive = measure([&] { return fhive(n, erasure_rate); });
-      auto thub = measure([&] { return fhub(n, erasure_rate); });
-      std::cout << std::fixed << std::setprecision(2)
-                << thive / thub << " "
-                << std::defaultfloat << std::setprecision(6) << std::flush;
+      else{
+        auto thive = measure([&] { return fhive(n, erasure_rate); });
+        auto thub = measure([&] { return fhub(n, erasure_rate); });
+        out << std::fixed << std::setprecision(2) << thive / thub;
+      }
+      std::cout << out.str() << " " << std::flush;
+      res.data.back().push_back(out.str());
     }
     std::cout << std::endl;
   }
+  return res;
 }
 
 template<typename Container>
@@ -273,27 +297,107 @@ struct sort
   }
 };
 
-int main()
+using table = std::vector<benchmark_result>;
+
+void write_table(const table& t, const char* filename)
 {
+  static std::size_t first_column_width = 15;
+  static std::size_t data_column_width = (max_size_exp + 1 - min_size_exp) * 5;
+  std::size_t        num_data_columns = t.size();
+  std::size_t        table_width = first_column_width + 2 + num_data_columns * (data_column_width + 2) + 1;
+
+  std::ofstream out(filename);
+
+  auto data_horizontal_line =
+    std::string(first_column_width + 2, ' ') + std::string(table_width - first_column_width - 2, '-');
+  auto table_horizontal_line = std::string(table_width, '-');
+
+  out << std::left;
+
+  out << data_horizontal_line << "\n";
+
+  out << "  " << std::setw(first_column_width) << " ";
+  out << std::setw(table_width - first_column_width - 3) 
+      << std::string("| sizeof(element): ") + std::to_string(ELEMENT_SIZE) << "|\n";
+
+  out << data_horizontal_line << "\n";
+
+  out << "  " << std::setw(first_column_width) << " ";
+  for(const benchmark_result& res: t) {
+    out << "| " << std::setw(data_column_width) << res.title;
+  }
+  out << "|\n";
+
+  out << data_horizontal_line << "\n";
+
+  out << "  " << std::setw(first_column_width) << " " ;
+  for(int i = 0; i < num_data_columns; ++i) {
+    out << "| " << std::setw(data_column_width) << "container size";
+  }
+  out << "|\n";
+
+  out << table_horizontal_line << "\n";
+
+  out << "| " << std::setw(first_column_width) << "erase rate";
+  for(int i = 0; i < num_data_columns; ++i) {
+    out << "| ";
+    for(auto j = min_size_exp; j <= max_size_exp; ++j) {
+          out << "1.E" << j << " ";
+    }
+  }
+  out << "|\n";
+
+  out << table_horizontal_line << "\n";
+  
+  std::size_t row = 0;
+  for(double erasure_rate = min_erasure_rate; 
+      erasure_rate <= max_erasure_rate; 
+      erasure_rate += erase_rate_inc, ++row) {
+    out << "| " << std::setw(first_column_width) << erasure_rate;
+    for(const benchmark_result& res: t) {
+      out << "| ";
+      for(const auto& x: res.data[row]) {
+        out << x << " ";
+      }
+    }
+    out << "|\n";
+  }
+
+  out << table_horizontal_line << "\n";
+}
+
+int main(int argc,char* argv[])
+{
+
+  if(argc < 2) {
+    std::cerr << "missing filename\n";
+    return EXIT_FAILURE;
+  }
+  const char* filename = argv[1];
+  
   try{
     using hive = plf::hive<element>;
     using hub = boost::container::hub<element>;
 
-    benchmark(
+    table t;
+
+    t.push_back(benchmark(
       "insert, erase, insert", 
-      create<hive>{}, create<hub>{});
-    benchmark(
-      "insert, erase, insert, destroy", 
-      create_and_destroy<hive>{}, create_and_destroy<hub>{});
-    benchmark(
+      create<hive>{}, create<hub>{}));
+    t.push_back(benchmark(
+      "ins, erase, ins, destroy", 
+      create_and_destroy<hive>{}, create_and_destroy<hub>{}));
+    t.push_back(benchmark(
       "for_each", 
-      for_each<hive>{}, for_each<hub>{});
-    benchmark(
+      for_each<hive>{}, for_each<hub>{}));
+    t.push_back(benchmark(
       "visit_all", 
-      for_each<hive>{}, visit_all<hub>{});
-    benchmark(
+      for_each<hive>{}, visit_all<hub>{}));
+    t.push_back(benchmark(
       "sort", 
-      sort<hive>{}, sort<hub>{});
+      sort<hive>{}, sort<hub>{}));
+
+    write_table(t, filename);
   }
   catch(const std::exception& e) {
     std::cerr << e.what() << std::endl;
